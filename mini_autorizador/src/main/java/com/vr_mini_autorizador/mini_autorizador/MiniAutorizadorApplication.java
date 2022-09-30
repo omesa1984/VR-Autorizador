9df27f235017a546d54eb293e32d352494b22cca
omesa198 <omesa1984@gmail.com>
2022-09-30 16:08:35 -0300

    Projeto criado

---
+++ mini_autorizador/src/main/java/com/vr_mini_autorizador/mini_autorizador/MiniAutorizadorApplication.java
@@ -0,0 +1,13 @@
+package com.vr_mini_autorizador.mini_autorizador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAutorizadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAutorizadorApplication.class, args);
+	}
+
+}
---
+++ mini_autorizador/src/test/java/com/vr_mini_autorizador/mini_autorizador/MiniAutorizadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vr_mini_autorizador.mini_autorizador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAutorizadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
